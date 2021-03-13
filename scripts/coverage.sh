90f0097baaf44f2f1bc277f7430ec07678d0aea4
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-03-13 13:58:21 +0200

    CMakeLists.txt refactoring.

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -8,11 +8,11 @@ for TEST in $1/*; do
     fi
 
     LLVM_PROFILE_FILE=$NAME.profraw $TEST >$NAME.tap
-    pytap2 html $NAME.tap >${NAME}-test-results.html
+    # pytap2 html $NAME.tap >${NAME}-test-results.html
 
     llvm-profdata-10 merge -sparse $NAME.profraw -o $NAME.profdata
     llvm-cov-10 show --format=html $TEST -instr-profile=$NAME.profdata -o $NAME
 
     x-www-browser ${NAME}-test-results.html &>/dev/null
-    x-www-browser $NAME/index.html &>/dev/null
+    # x-www-browser $NAME/index.html &>/dev/null
 done
