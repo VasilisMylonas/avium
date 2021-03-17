b30065e2f54ddbc934c0e8221f317385793fb93d
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-03-17 10:08:32 +0200

    Changes Doxygen/Sphinx integration.

--- cmake/FindNaturalDocs.cmake
+++ cmake/FindNaturalDocs.cmake
@@ -1,4 +1,4 @@
 include(FindPackageHandleStandardArgs)
 
 find_program(NATURAL_DOCS_EXECUTABLE NAMES natural-docs)
-find_package_handle_standard_args(NaturalDocs NATURAL_DOCS_EXECUTABLE)
+find_package_handle_standard_args(NATURAL_DOCS NATURAL_DOCS_EXECUTABLE)
