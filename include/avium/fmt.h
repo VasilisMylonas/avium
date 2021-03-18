5f8ffc15eaa36e625a767e152804ec76c091608f
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-03-18 20:36:20 +0200

    Documented fmt.h

--- include/avium/fmt.h
+++ include/avium/fmt.h
@@ -1,13 +1,11 @@
-/*
- * File: avium/fmt.h
- * Formatting utilities.
+/**
+ * @file avium/fmt.h
+ * @author Vasilis Mylonas <vasilismylonas@protonmail.com>
+ * @brief Formatted IO utilities.
+ * @version 0.1
+ * @date 2021-03-18
  *
- * About: Author
- * Vasilis Mylonas <vasilismylonas@protonmail.com>
- *
- * Section: License
- *
- * Copyright (C) 2021 Vasilis Mylonas
+ * @copyright Copyright (c) 2021 Vasilis Mylonas
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -16,11 +14,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef AVIUM_FMT_H
@@ -46,90 +44,70 @@
 #define AVM_FMT_TYPE         'T'
 #define AVM_FMT_SIZE         'Z'
 
-/*
- * Enum: NumericBase
- *
- * NB_BINARY  - Base 2 (00010000)
- * NB_OCTAL   - Base 8 (20)
- * NB_DECIMAL - Base 10 (16)
- * NB_HEX     - Base 16 (10)
- */
+/// Represents the base in which a number is represented.
 typedef enum {
+    /// Base 2 (00010000).
     NB_BINARY = 2,
+
+    /// Base 8 (20).
     NB_OCTAL = 8,
+
+    /// Base 10 (16).
     NB_DECIMAL = 10,
+
+    /// Base 16 (10).
     NB_HEX = 16,
 } NumericBase;
 
-/*
- * Function: AvmItoa
- * Converts a signed integer to a string.
+/**
+ * @brief Converts a signed integer to a string.
  *
- * Parameters:
- * value - The integer.
- *
- * Returns:
- * AvmString - The string representation.
+ * @param value The integer value.
+ * @return AvmString The string representation.
  */
 AVMAPI AvmString AvmItoa(_long value);
 
-/*
- * Function: AvmUtoa
- * Converts a unsigned integer to a string with a specified base.
+/**
+ * @brief Converts an unsigned integer to a string with a specified base.
  *
- * Parameters:
- * value - The unsigned integer.
- * base - The base of the number.
+ * @param value The unsigned integer value.
+ * @param base The base of the number.
  *
- * Returns:
- * AvmString - The string representation.
+ * @return AvmString The string representation.
  */
 AVMAPI AvmString AvmUtoa(ulong value, NumericBase base);
 
-/*
- * Function: AvmFtoa
- * Converts a double to a string.
+/**
+ * @brief Converts a double to a string.
  *
- * Parameters:
- * value - The double value.
- *
- * Returns:
- * AvmString - The string representation.
+ * @param value The double value.
+ * @return AvmString The string representation.
  */
 AVMAPI AvmString AvmFtoa(double value);
 
-/*
- * Function: AvmFtoa2
- * Converts a float to a string (custom implementation).
+/**
+ * @brief Converts a float to a string (custom implementation).
  *
- * Parameters:
- * value - The float value.
- *
- * Returns:
- * AvmString - The string representation.
+ * @param value The float value.
+ * @return AvmString The string representation.
  */
 AVMAPI AvmString AvmFtoa2(float value);
 
-/*
- * Function: AvmVScanf
- * Reads formatted output from stdin using a va_list.
+/**
+ * @brief Reads formatted output from stdin using a va_list.
  *
- * Parameters:
- * format - The format string.
- * args - The va_list containing pointers to variables to read to.
+ * @param format The format string.
+ * @param args The va_list containing pointers to variables to read to.
  */
 AVMAPI void AvmVScanf(str format, va_list args);
 
-/*
- * Function: AvmScanf
- * Reads formatted output from stdin.
+/**
+ * @brief Reads formatted output from stdin.
  *
- * Parameters:
- * format - The format string.
- * ... - Pointers to variables to read to.
+ * @param format The format string.
+ * @param ... Pointers to variables to read to.
  *
- * Note:
- * This function is inline.
+ * @note This function is inline.
  */
 inline void AvmScanf(str format, ...) {
     va_list args;
@@ -138,28 +116,23 @@ inline void AvmScanf(str format, ...) {
     va_end(args);
 }
 
-/*
- * Function: AvmSscanf
- * Reads formatted output from an AvmString using a va_list.
+/**
+ * @brief Reads formatted output from an AvmString using a va_list.
  *
- * Parameters:
- * string - The AvmString.
- * format - The format string.
- * args - The va_list containing pointers to variables to read to.
+ * @param string The AvmString.
+ * @param format The format string.
+ * @param args The va_list containing pointers to variables to read to.
  */
 AVMAPI void AvmVSscanf(AvmString string, str format, va_list args);
 
-/*
- * Function: AvmSscanf
- * Reads formatted output from an AvmString.
+/**
+ * @brief Reads formatted output from an AvmString.
  *
- * Parameters:
- * string - The AvmString.
- * format - The format string.
- * ... - Pointers to variables to read to.
+ * @param string The AvmString.
+ * @param format The format string.
+ * @param ... Pointers to variables to read to.
  *
- * Note:
- * This function is inline.
+ * @note This function is inline.
  */
 inline void AvmSscanf(AvmString string, str format, ...) {
     va_list args;
@@ -168,28 +141,23 @@ inline void AvmSscanf(AvmString string, str format, ...) {
     va_end(args);
 }
 
-/*
- * Function: AvmVFscanf
- * Reads formatted output from a stream using a va_list.
+/**
+ * @brief Reads formatted output from a stream using a va_list.
  *
- * Parameters:
- * handle - The stream handle.
- * format - The format string.
- * args - The va_list containing pointers to variables to read to.
+ * @param handle The stream handle.
+ * @param format The format string.
+ * @param args The va_list containing pointers to variables to read to.
  */
 AVMAPI void AvmVFscanf(void* handle, str format, va_list args);
 
-/*
- * Function: AvmFscanf
- * Reads formatted output from a stream.
+/**
+ * @brief Reads formatted output from a stream.
  *
- * Parameters:
- * handle - The stream handle.
- * format - The format string.
- * ... - Pointers to variables to read to.
+ * @param handle The stream handle.
+ * @param format The format string.
+ * @param ... Pointers to variables to read to.
  *
- * Note:
- * This function is inline.
+ * @note This function is inline.
  */
 inline void AvmFscanf(void* handle, str format, ...) {
     va_list args;
@@ -198,32 +166,25 @@ inline void AvmFscanf(void* handle, str format, ...) {
     va_end(args);
 }
 
-/*
- * Function: AvmVSprintf
- * Writes formatted output into a string using a va_list.
+/**
+ * @brief Writes formatted output into a string using a va_list.
  *
- * Parameters:
- * format - The format string.
- * args - The va_list containing values to insert into the format string.
+ * @param format The format string.
+ * @param args The va_list containing values to insert into the format string.
  *
- * Returns:
- * AvmString - The formatted string.
+ * @return AvmString The formatted string.
  */
 AVMAPI AvmString AvmVSprintf(str format, va_list args);
 
-/*
- * Function: AvmSprintf
- * Writes formatted output into a string.
+/**
+ * @brief Writes formatted output into a string.
  *
- * Parameters:
- * format - The format string.
- * ... - The values to insert into the format string.
+ * @param format The format string.
+ * @param ... The values to insert into the format string.
  *
- * Returns:
- * AvmString - The formatted string.
+ * @return AvmString The formatted string.
  *
- * Note:
- * This function is inline.
+ * @note This function is inline.
  */
 inline AvmString AvmSprintf(str format, ...) {
     va_list args;
@@ -233,28 +194,23 @@ inline AvmString AvmSprintf(str format, ...) {
     return s;
 }
 
-/*
- * Function: AvmVFprintf
- * Writes formatted output to a stream using a va_list.
+/**
+ * @brief Writes formatted output to a stream using a va_list.
  *
- * Parameters:
- * handle - The stream handle.
- * format - The format string.
- * args - The va_list containing the values to write.
+ * @param handle The stream handle.
+ * @param format The format string.
+ * @param args The va_list containing the values to write.
  */
 AVMAPI void AvmVFprintf(void* handle, str format, va_list args);
 
-/*
- * Function: AvmFprintf
- * Writes formatted output to a stream.
+/**
+ * @brief Writes formatted output to a stream.
  *
- * Parameters:
- * handle - The stream handle.
- * format - The format string.
- * ... - The values to write.
+ * @param handle The stream handle.
+ * @param format The format string.
+ * @param ... The values to write.
  *
- * Note:
- * This function is inline.
+ * @note This function is inline.
  */
 inline void AvmFprintf(void* handle, str format, ...) {
     va_list args;
@@ -263,26 +219,21 @@ inline void AvmFprintf(void* handle, str format, ...) {
     va_end(args);
 }
 
-/*
- * Function: AvmVPrintf
- * Writes formatted output to stdout using a va_list.
+/**
+ * @brief Writes formatted output to stdout using a va_list.
  *
- * Parameters:
- * format - The format string.
- * args - The va_list containing the values to write.
+ * @param format The format string.
+ * @param args The va_list containing the values to write.
  */
 AVMAPI void AvmVPrintf(str format, va_list args);
 
-/*
- * Function: AvmPrintf
- * Writes formatted output to stdout.
+/**
+ * @brief Writes formatted output to stdout.
  *
- * Parameters:
- * format - The format string.
- * ... - The values to write.
+ * @param format The format string.
+ * @param ... The values to write.
  *
- * Note:
- * This function is inline.
+ * @note This function is inline.
  */
 inline void AvmPrintf(str format, ...) {
     va_list args;
@@ -291,26 +242,21 @@ inline void AvmPrintf(str format, ...) {
     va_end(args);
 }
 
-/*
- * Function: AvmVErrorf
- * Writes formatted output to stderr using a va_list.
+/**
+ * @brief Writes formatted output to stderr using a va_list.
  *
- * Parameters:
- * format - The format string.
- * args - The va_list containing the values to write.
+ * @param format The format string.
+ * @param args The va_list containing the values to write.
  */
 AVMAPI void AvmVErrorf(str format, va_list args);
 
-/*
- * Function: AvmErrorf
+/**
  * Writes formatted output to stderr.
  *
- * Parameters:
- * format - The format string.
- * ... - The values to write.
+ * @param format The format string.
+ * @param ... The values to write.
  *
- * Note:
- * This function is inline.
+ * @note This function is inline.
  */
 inline void AvmErrorf(str format, ...) {
     va_list args;
