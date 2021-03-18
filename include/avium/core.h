24ac0436ce548784b1fbe08fa09af16b38f6211f
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-03-18 20:23:08 +0200

    Merged options.h

--- include/avium/core.h
+++ include/avium/core.h
@@ -479,4 +479,37 @@ AVMAPI uint AvmVersionGetPatch(AvmVersion self);
  */
 AVMAPI char AvmVersionGetTag(AvmVersion self);
 
+/// Represents a command line option.
+typedef struct {
+    /// The long option in the form of --help, but without the --.
+    str longOption;
+
+    /// The short option in the form of -h, but without the -.
+    char shortOption;
+} AvmOption;
+
+/**
+ * @brief Determines whether the specified option is present.
+ *
+ * @param argc The argc parameter from main.
+ * @param argv The argv parameter from main.
+ * @param option The desired option.
+ *
+ * @return true The specified option was present.
+ * @return false The specified option was not present.
+ */
+AVMAPI bool AvmHasOption(int argc, str* argv, AvmOption option);
+
+/**
+ * @brief Retrieves a command line option.
+ *
+ * @param argc The argc parameter from main.
+ * @param argv The argv parameter from main.
+ * @param option The desired option.
+ *
+ * @return AvmSome(str) The command line option value.
+ * @return AvmNone The option was not present.
+ */
+AVMAPI AvmOptional AvmGetOption(int argc, str* argv, AvmOption option);
+
 #endif  // AVIUM_CORE_H
