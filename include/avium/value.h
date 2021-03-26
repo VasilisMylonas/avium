1cda3da716511905f11b37ec6cc3283045a90e7a
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-03-26 09:57:52 +0200

    Added AvmValue.

---
+++ include/avium/value.h
@@ -0,0 +1,36 @@
+#ifndef AVIUM_VALUE_H
+#define AVIUM_VALUE_H
+
+#include "avium/prologue.h"
+
+typedef enum {
+    ValueKindBool = 0,
+    ValueKindInt,
+    ValueKindUInt,
+    ValueKindFloat,
+    ValueKindStr,
+    ValueKindChar,
+} AvmValueKind;
+
+AVM_CLASS(AvmValue, object, {
+    union {
+        bool _asBool;
+        double _asFloat;
+        ulong _asUInt;
+        _long _asInt;
+        str _asStr;
+        char _asChar;
+    };
+    AvmValueKind _kind;
+});
+
+AVMAPI AvmValue AvmValueFromBool(bool value);
+AVMAPI AvmValue AvmValueFromChar(char value);
+AVMAPI AvmValue AvmValueFromFloat(double value);
+AVMAPI AvmValue AvmValueFromUInt(ulong value);
+AVMAPI AvmValue AvmValueFromInt(_long value);
+AVMAPI AvmValue AvmValueFromStr(str value);
+
+static_assert_s(sizeof(AvmValue) == 24)
+
+#endif  // AVIUM_VALUE_H
