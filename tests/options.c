2302be077c841fddb4d5ef27a94d212eef9040aa
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-03-26 21:01:18 +0200

    Removed a comment.

--- tests/options.c
+++ tests/options.c
@@ -6,7 +6,6 @@ static object TestInit(int argc, str argv[]) {
     return AvmOptionParserNew(argc, argv);
 }
 
-// AvmOptionParserAddStandardOptions(state);
 static void TestFini(object state) {
     AvmObjectDestroy(state);
     free(state);
@@ -20,12 +19,6 @@ static void TestUsage(object state) {
                              ValueKindBool);
     AvmOptionParserAddOptionEx(state, "version", 'v',
                                "Show version information.", ValueKindBool);
-
-    // AvmArrayList(AvmValue) values = AvmOptionParserParse(state);
-
-    // for (size_t i = 0; i < values._length; i++) {
-    //     AvmPrintf("%v\n", &values._items[i]);
-    // }
 
     AvmOptionParserShowUsage(state, "Do something useful.");
 }
