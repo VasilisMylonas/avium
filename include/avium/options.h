b50523486db87a0b488a1f5e9d0f978fe8b63bd9
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-04-06 09:31:29 +0300

    options.h banner.

--- include/avium/options.h
+++ include/avium/options.h
@@ -1,3 +1,26 @@
+/**
+ * @file avium/options.h
+ * @author Vasilis Mylonas <vasilismylonas@protonmail.com>
+ * @brief Command line option parsing.
+ * @version 0.2
+ * @date 2021-04-06
+ *
+ * @copyright Copyright (c) 2021 Vasilis Mylonas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef AVIUM_OPTIONS_H
 #define AVIUM_OPTIONS_H
 
