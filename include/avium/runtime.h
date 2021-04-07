54585916f60335828c52ceac18e4b748422bb6a3
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-04-07 14:57:22 +0300

    VTable changes.

--- include/avium/runtime.h
+++ include/avium/runtime.h
@@ -6,15 +6,20 @@
 /// Represents an entry on the virtual function entry.
 typedef enum {
     FUNC_DTOR = 0,
-    FUNC_GET_LENGTH,
-    FUNC_GET_CAPACITY,
     FUNC_TO_STRING,
     FUNC_CLONE,
     FUNC_EQUALS,
-    FUNC_READ,
+
+    // AvmCollection
+    FUNC_GET_LENGTH = 16,
+    FUNC_GET_CAPACITY,
+
+    // AvmStream
+    FUNC_READ = 16,
     FUNC_WRITE,
-    FUNC_READ_STRING,
-    FUNC_WRITE_STRING,
+    FUNC_SEEK,
+    FUNC_FLUSH,
+    FUNC_GET_POSITION
 } AvmFunctionEntry;
 
 /// Enables signal catching.
