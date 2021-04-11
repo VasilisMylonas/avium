b448436517fa804728eb3a05d113edd8545591d7
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-04-11 22:29:50 +0300

    Added coveralls github action.

--- scripts/gcovr.sh
+++ scripts/gcovr.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-GCDA_TEST_FILES=$(find ./CMakeFiles/*.dir/*.gcda)
-GCDA_FILES=$(find ../src/CMakeFiles/*.dir/*.gcda)
+GCDA_TEST_FILES=$(find tests/CMakeFiles/*.dir/*.gcda)
+GCDA_FILES=$(find src/CMakeFiles/*.dir/*.gcda)
 
-mkdir -p ../coverage
-geninfo $GCDA_FILES $GCDA_TEST_FILES -b ../../src -o ../coverage/coverage.info
-genhtml ../coverage/coverage.info -o ../coverage
+geninfo $GCDA_FILES $GCDA_TEST_FILES -b . -o coverage/lcov.info
+# genhtml ../coverage/coverage.info -o ../coverage
