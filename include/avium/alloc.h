6ff27b51e418d2854b8e7c8e7f1e60aadad5c347
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-04-11 09:56:21 +0300

    Cleanup on alloc.h and alloc.c

--- include/avium/alloc.h
+++ include/avium/alloc.h
@@ -1,10 +1,35 @@
+/**
+ * @file avium/alloc.h
+ * @author Vasilis Mylonas <vasilismylonas@protonmail.com>
+ * @brief Object allocation API.
+ * @version 0.2
+ * @date 2021-04-11
+ *
+ * @copyright Copyright (c) 2021 Vasilis Mylonas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef AVIUM_ALLOC_H
 #define AVIUM_ALLOC_H
 
 #include "avium/types.h"
 
-AVMAPI object AvmAlloc(size_t size, object data);
+AVMAPI object AvmObjectAlloc(size_t size, object data);
+AVMAPI void AvmObjectDealloc(object self);
 
-#define heapalloc(T, ...) AvmAlloc(sizeof(T), (T[1]){__VA_ARGS__})
+#define heapalloc(T, ...) AvmObjectAlloc(sizeof(T), (T[1]){__VA_ARGS__})
+#define heapfree(object)  AvmObjectDealloc(object)
 
 #endif  // AVIUM_ALLOC_H
--- src/alloc.c
+++ src/alloc.c
@@ -3,10 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-object AvmAlloc(size_t size, object data) {
+object AvmObjectAlloc(size_t size, object data) {
     object memory = malloc(size);
     if (data != NULL) {
         memcpy(memory, data, size);
     }
     return memory;
+}
+
+void AvmObjectDealloc(object memory) {
+    if (memory != NULL) {
+        AvmObjectDestroy(memory);
+        free(memory);
+    }
 }
