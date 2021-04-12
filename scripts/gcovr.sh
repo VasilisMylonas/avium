e0c5e38ca6c033beb1c98be5f92e54d6390a4f2d
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-04-12 16:05:33 +0300

    Scripts rework.

--- scripts/gcovr.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-GCDA_TEST_FILES=$(find tests/CMakeFiles/*.dir/*.gcda)
-GCDA_FILES=$(find src/CMakeFiles/*.dir/*.gcda)
-
-geninfo $GCDA_FILES $GCDA_TEST_FILES -b . -o coverage/lcov.info
-# genhtml ../coverage/coverage.info -o ../coverage
