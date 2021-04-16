0e509007e4bfab68553adf381e7b6b821df56be3
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-04-16 22:05:58 +0300

    Libgc not needed.

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -4,7 +4,7 @@ BUILD_DIR=$1
 BUILD_TYPE=$2
 
 cmake -E make_directory $BUILD_DIR
-cmake -S . -B $BUILD_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+cmake -S . -B $BUILD_DIR -DAVM_USE_GC=OFF -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 cmake --build $BUILD_DIR --target HtmlDocs
 
 mkdir -p ./site
