27c9dbd63078f1c455f1777759a12e523712f4c8
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-04-22 17:08:00 +0300

    Removed libgc from install-deps.sh

--- ci/install-deps.sh
+++ ci/install-deps.sh
@@ -4,7 +4,7 @@
 sudo apt update &&
 
 # Install dependencies.
-sudo apt install -y cmake doxygen python3 python3-pip lcov libgc1c2 libgc-dev &&
+sudo apt install -y cmake doxygen python3 python3-pip lcov &&
 
 # Install python packages.
 sudo pip3 install sphinx breathe sphinx-rtd-theme
