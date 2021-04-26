3d6ea03b62e4c6ea176afa5c9c1f1949f2e3bfdb
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-04-26 10:48:15 +0300

    Resources made it to header again.

--- include/avium/private/resources.h
+++ include/avium/private/resources.h
@@ -7,28 +7,31 @@
 #define BACKTRACE_MAX_SYMBOLS 128
 #define READ_LINE_CAPACITY    32
 
-extern const str LongMinRepr;
-extern const str SelfNullMsg;
-extern const str ContentsNullMsg;
-extern const str OtherNullMsg;
-extern const str SubstringNullMsg;
-extern const str FormatNullMsg;
-extern const str ArgsNullMsg;
-extern const str HandleNullMsg;
-extern const str SourceNullMsg;
-extern const str DestinationNullMsg;
-extern const str NumericBaseOutOfRangeMsg;
-extern const str StringNullMsg;
-extern const str BufferNullMsg;
-extern const str IndexOutOfRange;
-extern const str FunctionNullMsg;
-extern const str InvalidValueCastMsg;
-extern const str InvalidValueKindMsg;
-extern const str NoBacktraceMsg;
-extern const str InvalidPtrDerefMsg;
-extern const str IllegalInstructionMsg;
-extern const str ArithmeticExceptionMsg;
-extern const str InvalidOriginMsg;
-extern const str PathNullMsg;
+#define NULL_PARAM_STR(name)  "Parameter `" name "` was `NULL`."
+#define RANGE_PARAM_STR(name) "Parameter `" name "` was out of range."
+
+static const str LongMinRepr = "-9223372036854775808";
+static const str SelfNullMsg = NULL_PARAM_STR("self");
+static const str ContentsNullMsg = NULL_PARAM_STR("contents");
+static const str OtherNullMsg = NULL_PARAM_STR("other");
+static const str SubstringNullMsg = NULL_PARAM_STR("substring");
+static const str FormatNullMsg = NULL_PARAM_STR("format");
+static const str ArgsNullMsg = NULL_PARAM_STR("args");
+static const str HandleNullMsg = NULL_PARAM_STR("handle");
+static const str SourceNullMsg = NULL_PARAM_STR("source");
+static const str DestinationNullMsg = NULL_PARAM_STR("destination");
+static const str NumericBaseOutOfRangeMsg = RANGE_PARAM_STR("numericBase");
+static const str StringNullMsg = NULL_PARAM_STR("string");
+static const str BufferNullMsg = NULL_PARAM_STR("buffer");
+static const str IndexOutOfRange = "The specified index was out of range.";
+static const str FunctionNullMsg = NULL_PARAM_STR("function");
+static const str InvalidValueCastMsg = "Invalid AvmValue cast.";
+static const str InvalidValueKindMsg = "Invalid AvmValue kind.";
+static const str NoBacktraceMsg = "No backtrace is available.";
+static const str InvalidPtrDerefMsg = "Invalid pointer dereference.";
+static const str IllegalInstructionMsg = "Illegal instruction.";
+static const str ArithmeticExceptionMsg = "Arithmetic exception.";
+static const str InvalidOriginMsg = "Parameter `origin` was invalid.";
+static const str PathNullMsg = NULL_PARAM_STR("path");
 
 #endif  // AVIUM_RESOURCES_H
