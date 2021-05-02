2274498049f17f7fff03fad4f7188b70f0d95496
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-05-02 19:56:49 +0300

    Removed content from module.c

--- src/module.c
+++ src/module.c
@@ -1,12 +1,1 @@
 const int __AvmDummy = 0;
-
-#include <avium/reflect.h>
-
-void AvmRuntimeRunTests(str program) {
-    AvmEnableExceptions();
-
-    (void)program;
-    // TODO
-
-    AvmDisableExceptions();
-}
