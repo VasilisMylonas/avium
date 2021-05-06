75691f80b58cb8875fb2486f609c6ea5cb7beab8
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-05-07 00:45:09 +0300

    Add fake constants for emulation.

--- extras/include/dlfcn.h
+++ extras/include/dlfcn.h
@@ -2,3 +2,12 @@ extern void* dlopen(const char* filename, int mode);
 extern int dlclose(void* handle);
 extern void* dlsym(void* handle, const char* name);
 extern char* dlerror(void);
+
+#define RTLD_LAZY         0
+#define RTLD_NOW          0
+#define RTLD_BINDING_MASK 0
+#define RTLD_NOLOAD       0
+#define RTLD_DEEPBIND     0
+#define RTLD_GLOBAL       0
+#define RTLD_LOCAL        0
+#define RTLD_NODELETE     0
