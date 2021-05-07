1bdc44212dc76f4bcbceb78d87c9bfef5474b641
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-05-07 18:57:37 +0300

    Changed assert macros.

--- include/avium/testing.h
+++ include/avium/testing.h
@@ -27,18 +27,19 @@
 #include "avium/runtime.h"
 
 // TODO: These should be toggable via some config mechanism.
-#define pre  if (true)
+#define pre if (true)
 #define post if (true)
 
 #include <assert.h>
 
-#define AssertTrue(condition) \
-    __AvmAssert(condition, __FILE__, __LINE__, #condition)
-
-#define AssertFalse(condition) AssertTrue(!condition)
-
-#define AssertEq(x, y) \
-    _Generic((x), byte                 \
+#ifdef NDEBUG
+#define assert_eq(x, y) ((void)0)
+#define assert_ne(x, y) ((void)0)
+#define assert_gt(x, y) ((void)0)
+#define assert_lt(x, y) ((void)0)
+#else
+#define assert_eq(x, y)                  \
+    _Generic((x), byte                   \
              : __AvmAssertEqUint, ushort \
              : __AvmAssertEqUint, uint   \
              : __AvmAssertEqUint, ulong  \
@@ -47,11 +48,11 @@
              : __AvmAssertEqInt, int     \
              : __AvmAssertEqInt, _long   \
              : __AvmAssertEqInt, str     \
-             : __AvmAssertEqStr, char*   \
+             : __AvmAssertEqStr, char *  \
              : __AvmAssertEqStr)(x, y, __FILE__, __LINE__, #x, #y)
 
-#define AssertNe(x, y) \
-    _Generic((x), byte                 \
+#define assert_ne(x, y)                  \
+    _Generic((x), byte                   \
              : __AvmAssertNeUint, ushort \
              : __AvmAssertNeUint, uint   \
              : __AvmAssertNeUint, ulong  \
@@ -60,11 +61,11 @@
              : __AvmAssertNeInt, int     \
              : __AvmAssertNeInt, _long   \
              : __AvmAssertNeInt, str     \
-             : __AvmAssertNeStr, char*   \
+             : __AvmAssertNeStr, char *  \
              : __AvmAssertNeStr)(x, y, __FILE__, __LINE__, #x, #y)
 
-#define AssertGt(x, y) \
-    _Generic((x), byte                 \
+#define assert_gt(x, y)                  \
+    _Generic((x), byte                   \
              : __AvmAssertGtUint, ushort \
              : __AvmAssertGtUint, uint   \
              : __AvmAssertGtUint, ulong  \
@@ -73,64 +74,22 @@
              : __AvmAssertGtInt, int     \
              : __AvmAssertGtInt, _long   \
              : __AvmAssertGtInt, str     \
-             : __AvmAssertGtStr, char*  \
+             : __AvmAssertGtStr, char *  \
              : __AvmAssertGtStr)(x, y, __FILE__, __LINE__, #x, #y)
 
-#define AssertLt(x, y) \
-    _Generic((x), byte                 \
-             : __AvmAssertLtUint, ushort \
-             : __AvmAssertLtUint, uint   \
-             : __AvmAssertLtUint, ulong  \
-             : __AvmAssertLtUint, char   \
-             : __AvmAssertLtInt, short   \
-             : __AvmAssertLtInt, int     \
-             : __AvmAssertLtInt, _long   \
-             : __AvmAssertLtInt, str     \
-             : __AvmAssertLtStr, char*  \
-             : __AvmAssertLtStr)(x, y, __FILE__, __LINE__, #x, #y)
-
-#define AssertInRange(min, max, value)                                     \
-    _Generic((x), byte                                                     \
-             : __AvmAssertInRangeUint, ushort                              \
-             : __AvmAssertInRangeUint, uint                                \
-             : __AvmAssertInRangeUint, ulong                               \
-             : __AvmAssertInRangeUint, char                                \
-             : __AvmAssertInRangeInt, short                                \
-             : __AvmAssertInRangeInt, int                                  \
-             : __AvmAssertInRangeInt, _long                                \
-             : __AvmAssertInRangeInt)(value, min, max, __FILE__, __LINE__, \
-                                      #value)
-
-#define AssertNull(pointer) \
-    __AvmAssertNull(pointer, __FILE__, __LINE__, #pointer)
-
-#define AssertNotNull(pointer) \
-    __AvmAssertNotNull(pointer, __FILE__, __LINE__, #pointer)
-
-#define AssertMemEq(x, y, length) \
-    __AvmAssertMemEq((byte*)(x), (byte*)(y), length, __FILE__, __LINE__, #x, #y)
-
-#define test int main()
+#define assert_lt(x, y) assert_gt(y, x)
+#endif // NDEBUG
 
 #ifndef DOXYGEN
-AVMAPI void __AvmAssert(bool, str, uint, str);
 AVMAPI void __AvmAssertEqInt(_long, _long, str, uint, str, str);
 AVMAPI void __AvmAssertNeInt(_long, _long, str, uint, str, str);
 AVMAPI void __AvmAssertGtInt(_long, _long, str, uint, str, str);
-AVMAPI void __AvmAssertLtInt(_long, _long, str, uint, str, str);
 AVMAPI void __AvmAssertEqUint(ulong, ulong, str, uint, str, str);
 AVMAPI void __AvmAssertNeUint(ulong, ulong, str, uint, str, str);
 AVMAPI void __AvmAssertGtUint(ulong, ulong, str, uint, str, str);
-AVMAPI void __AvmAssertLtUint(ulong, ulong, str, uint, str, str);
 AVMAPI void __AvmAssertEqStr(str, str, str, uint, str, str);
 AVMAPI void __AvmAssertNeStr(str, str, str, uint, str, str);
 AVMAPI void __AvmAssertGtStr(str, str, str, uint, str, str);
-AVMAPI void __AvmAssertLtStr(str, str, str, uint, str, str);
-AVMAPI void __AvmAssertMemEq(byte*, byte*, size_t, str, uint, str, str);
-AVMAPI void __AvmAssertInRangeInt(_long, _long, _long, str, uint, str);
-AVMAPI void __AvmAssertInRangeUint(ulong, ulong, ulong, str, uint, str);
-AVMAPI void __AvmAssertNull(void*, str, uint, str);
-AVMAPI void __AvmAssertNotNull(void*, str, uint, str);
-#endif  // DOXYGEN
+#endif // DOXYGEN
 
-#endif  // AVIUM_TESTING_H
+#endif // AVIUM_TESTING_H
--- src/avm.core/testing.c
+++ src/avm.core/testing.c
@@ -2,12 +2,20 @@
 
 #include <string.h>
 
-static void PrintLocation(str file, uint line) {
-    AvmErrorf("(%s:%u) ", file, line);
+static void PrintLocation(str file, uint line)
+{
+    const size_t length = strlen(file) + 5;
+    AvmErrorf("%s:%u\n", file, line);
+    for (size_t i = 0; i < length; i++)
+    {
+        AvmErrorf("-");
+    }
+    AvmErrorf("\n");
 }
 
 #define ASSERT_INT_BODY(op)                                       \
-    if (x op y) {                                                 \
+    if (x op y)                                                   \
+    {                                                             \
         return;                                                   \
     }                                                             \
     PrintLocation(file, line);                                    \
@@ -15,7 +23,8 @@ static void PrintLocation(str file, uint line) {
     AvmErrorf("Got:\n    %s: %i\n    %s: %i\n\n", xName, x, yName, y)
 
 #define ASSERT_UINT_BODY(op)                                      \
-    if (x op y) {                                                 \
+    if (x op y)                                                   \
+    {                                                             \
         return;                                                   \
     }                                                             \
     PrintLocation(file, line);                                    \
@@ -23,147 +32,61 @@ static void PrintLocation(str file, uint line) {
     AvmErrorf("Got:\n    %s: %u\n    %s: %u\n\n", xName, x, yName, y)
 
 #define ASSERT_STR_BODY(op)                                           \
-    if (strcmp(x, y) op 0) {                                          \
+    if (strcmp(x, y) op 0)                                            \
+    {                                                                 \
         return;                                                       \
     }                                                                 \
     PrintLocation(file, line);                                        \
     AvmErrorf("Assertion failed: [%s] " #op " [%s]\n", xName, yName); \
     AvmErrorf("Got:\n    %s: %s\n    %s: %s\n\n", xName, x, yName, y)
 
-void __AvmAssert(bool condition, str file, uint line, str expression) {
-    if (condition) {
-        return;
-    }
-
-    PrintLocation(file, line);
-    AvmErrorf("Assertion failed: %s\n\n", expression);
-}
-
 void __AvmAssertEqInt(_long x, _long y, str file, uint line, str xName,
-                      str yName) {
+                      str yName)
+{
     ASSERT_INT_BODY(==);
 }
 
-void __AvmAssertNeInt(_long x, _long y, str file, uint line, str xName,
-                      str yName) {
-    ASSERT_INT_BODY(!=);
-}
-
-void __AvmAssertGtInt(_long x, _long y, str file, uint line, str xName,
-                      str yName) {
-    ASSERT_INT_BODY(>);
-}
-
-void __AvmAssertLtInt(_long x, _long y, str file, uint line, str xName,
-                      str yName) {
-    ASSERT_INT_BODY(<);
-}
-
 void __AvmAssertEqUint(ulong x, ulong y, str file, uint line, str xName,
-                       str yName) {
-    if (x == y) {
-        return;
-    }
-
+                       str yName)
+{
     ASSERT_UINT_BODY(==);
 }
 
-void __AvmAssertNeUint(ulong x, ulong y, str file, uint line, str xName,
-                       str yName) {
-    ASSERT_UINT_BODY(!=);
-}
-
-void __AvmAssertGtUint(ulong x, ulong y, str file, uint line, str xName,
-                       str yName) {
-    ASSERT_UINT_BODY(>);
-}
-
-void __AvmAssertLtUint(ulong x, ulong y, str file, uint line, str xName,
-                       str yName) {
-    ASSERT_UINT_BODY(<);
-}
-
-void __AvmAssertEqStr(str x, str y, str file, uint line, str xName, str yName) {
+void __AvmAssertEqStr(str x, str y, str file, uint line, str xName, str yName)
+{
     ASSERT_STR_BODY(==);
 }
 
-void __AvmAssertNeStr(str x, str y, str file, uint line, str xName, str yName) {
+void __AvmAssertNeInt(_long x, _long y, str file, uint line, str xName,
+                      str yName)
+{
+    ASSERT_INT_BODY(!=);
+}
+
+void __AvmAssertNeUint(ulong x, ulong y, str file, uint line, str xName,
+                       str yName)
+{
+    ASSERT_UINT_BODY(!=);
+}
+
+void __AvmAssertNeStr(str x, str y, str file, uint line, str xName, str yName)
+{
     ASSERT_STR_BODY(!=);
 }
 
-void __AvmAssertGtStr(str x, str y, str file, uint line, str xName, str yName) {
+void __AvmAssertGtInt(_long x, _long y, str file, uint line, str xName,
+                      str yName)
+{
+    ASSERT_INT_BODY(>);
+}
+
+void __AvmAssertGtUint(ulong x, ulong y, str file, uint line, str xName,
+                       str yName)
+{
+    ASSERT_UINT_BODY(>);
+}
+
+void __AvmAssertGtStr(str x, str y, str file, uint line, str xName, str yName)
+{
     ASSERT_STR_BODY(>);
-}
-
-void __AvmAssertLtStr(str x, str y, str file, uint line, str xName, str yName) {
-    ASSERT_STR_BODY(<);
-}
-
-void __AvmAssertNull(void* pointer, str file, uint line, str name) {
-    if (pointer == NULL) {
-        return;
-    }
-
-    PrintLocation(file, line);
-    AvmErrorf("Assertion failed: %s == NULL\n\n", name);
-}
-
-void __AvmAssertNotNull(void* pointer, str file, uint line, str name) {
-    if (pointer != NULL) {
-        return;
-    }
-
-    PrintLocation(file, line);
-    AvmErrorf("Assertion failed: %s != NULL\n\n", name);
-}
-
-void __AvmAssertInRangeInt(_long value, _long _min, _long _max, str file,
-                           uint line, str name) {
-    if (_min <= value && value <= _max) {
-        return;
-    }
-
-    PrintLocation(file, line);
-    AvmErrorf("Assertion failed: %i <= %s <= %i\n\n", _min, name, _max);
-}
-
-void __AvmAssertInRangeUint(ulong value, ulong _min, ulong _max, str file,
-                            uint line, str name) {
-    if (_min <= value && value <= _max) {
-        return;
-    }
-
-    PrintLocation(file, line);
-    AvmErrorf("Assertion failed: %u <= %s <= %u\n\n", _min, name, _max);
-}
-
-void __AvmAssertMemEq(byte* x, byte* y, size_t length, str file, uint line,
-                      str xName, str yName) {
-    if (memcmp(x, y, length) == 0) {
-        return;
-    }
-
-    PrintLocation(file, line);
-    AvmErrorf("Assertion failed: [%s] == [%s]\n", xName, yName);
-    AvmErrorf("Memory was:\n    ");
-    for (size_t i = 0; i < length; i++) {
-        if (x[i] < 10) {  // One digit
-            AvmErrorf("%i    ", x[i]);
-        } else if (x[i] < 100) {
-            AvmErrorf("%i   ", x[i]);
-        } else {
-            AvmErrorf("%i  ", x[i]);
-        }
-    }
-    AvmErrorf("\n    ");
-    for (size_t i = 0; i < length; i++) {
-        if (y[i] < 10) {
-            AvmErrorf("%i    ", y[i]);
-        } else if (y[i] < 100) {
-            AvmErrorf("%i   ", y[i]);
-        } else {
-            AvmErrorf("%i  ", y[i]);
-        }
-    }
-    AvmErrorf("\n\n");
 }
