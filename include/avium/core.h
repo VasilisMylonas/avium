46c341edae56d56868ab6981d3ea57b21de0416b
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-05-09 19:31:14 +0300

    Merged version.h

--- include/avium/core.h
+++ include/avium/core.h
@@ -32,6 +32,13 @@
 #pragma weak AvmDealloc
 #endif
 
+/// A type representing a software version in the format: N.N.N
+AVM_CLASS(AvmVersion, object, {
+    ushort Major; ///< The major version.
+    ushort Minor; ///< The minor version.
+    ushort Patch; ///< The patch version.
+});
+
 /// Refers to the base type in a function with a self parameter.
 #define base (&self->_base)
 
@@ -64,6 +71,13 @@ AVMAPI const AvmType* AvmObjectGetType(object self);
  * @return true if the two objects are equal, otherwise false.
  */
 AVMAPI bool AvmObjectEquals(object self, object other);
+
+/**
+ * @brief Return an AvmVersion indicating the current runtime version.
+ *
+ * @return The current runtime version.
+ */
+AVMAPI AvmVersion AvmRuntimeGetVersion(void);
 
 /**
  * @brief Destroys an object.
@@ -168,5 +182,20 @@ AVMAPI void AvmVErrorf(str format, va_list args);
 AVMAPI void AvmScanf(str format, ...);
 AVMAPI void AvmPrintf(str format, ...);
 AVMAPI void AvmErrorf(str format, ...);
+
+/**
+ * @brief Creates a new AvmVersion instance.
+ *
+ * @param major The version major number (incremented at breaking changes).
+ * @param minor The version minor number (incremented at non-breaking changes).
+ * @param patch The version patch number (incremented at bug fixes).
+ *
+ * @return The created instance.
+ */
+AVMAPI AvmVersion AvmVersionFrom(ushort major, ushort minor, ushort patch);
+
+#ifndef DOXYGEN
+static_assert_s(sizeof(AvmVersion) == AVM_VERSION_SIZE);
+#endif // DOXYGEN
 
 #endif // AVIUM_CORE_H
