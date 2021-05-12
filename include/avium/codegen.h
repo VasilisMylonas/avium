b65c65bc6a5a6b8167031173f272fe9c4469c0b1
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-05-12 19:40:34 +0300

    Added AvmTypeBuilder.

--- include/avium/codegen.h
+++ include/avium/codegen.h
@@ -1,9 +1,82 @@
+/**
+ * @file avium/codegen.h
+ * @author Vasilis Mylonas <vasilismylonas@protonmail.com>
+ * @brief Runtime code generation utilities.
+ * @version 0.2
+ * @date 2021-05-12
+ *
+ * @copyright Copyright (c) 2021 Vasilis Mylonas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef AVIUM_CODEGEN_H
 #define AVIUM_CODEGEN_H
 
 #include "avium/core.h"
 
 #ifdef AVM_USE_REFLECT
+
+/// Represents an object destructor.
+typedef void (*AvmDtor)(object);
+
+/// A helper object for creating AvmType instances.
+AVM_CLASS(AvmTypeBuilder, AvmType, { uint _reserved; });
+
+/**
+ * @brief Creates an AvmTypeBuilder with a base type.
+ *
+ * @pre Parameter @p type must be not null.
+ *
+ * @param type The base type.
+ * @return The created AvmTypeBuilder.
+ */
+AVMAPI AvmTypeBuilder* AvmTypeBuilderBasedOn(const AvmType* type);
+
+/**
+ * @brief Sets the name for the AvmType to be created by an AvmTypeBuilder.
+ *
+ * @pre Parameter @p self must be not null.
+ * @pre Parameter @p name must be not null.
+ *
+ * @param self The AvmTypeBuilder instance.
+ * @param name The name for the type.
+ */
+AVMAPI void AvmTypeBuilderSetName(AvmTypeBuilder* self, str name);
+
+/**
+ * @brief Sets the destructor for the AvmType to be created by an
+ * AvmTypeBuilder.
+ *
+ * @pre Parameter @p self must be not null.
+ * @pre Parameter @p dtor must be not null.
+ *
+ * @param self The AvmTypeBuilder instance.
+ * @param dtor The destructor for the type.
+ */
+AVMAPI void AvmTypeBuilderSetDtor(AvmTypeBuilder* self, AvmDtor dtor);
+
+/**
+ * @brief Adds a member to an AvmTypeBuilder.
+ *
+ * @pre Parameter @p self must be not null.
+ * @pre Parameter @p type must be not null.
+ *
+ * @param self The AvmTypeBuilder instance.
+ * @param type The type of the member to be added.
+ */
+AVMAPI void AvmTypeBuilderAddMember(AvmTypeBuilder* self, const AvmType* type);
 
 #endif // AVM_USE_REFLECT
 
---
+++ src/avm.reflect/codegen.c
@@ -0,0 +1,64 @@
+#include "avium/codegen.h"
+#include "avium/testing.h"
+
+static void AvmTypeBuilderDestroy(AvmTypeBuilder* self)
+{
+    pre
+    {
+        assert(self != NULL);
+    }
+
+    AvmDealloc(self);
+}
+
+AVM_TYPE(AvmTypeBuilder,
+         object,
+         {[FnEntryDtor] = (AvmFunction)AvmTypeBuilderDestroy});
+
+AvmTypeBuilder* AvmTypeBuilderBasedOn(AvmType* type)
+{
+    pre
+    {
+        assert(type != NULL);
+    }
+
+    AvmTypeBuilder* self = AvmAlloc(sizeof(AvmTypeBuilder));
+    self->_type = typeid(AvmTypeBuilder);
+    base->_baseType = type;
+    base->_size = AvmTypeGetSize(type);
+    base->_vptr[FnEntryDtor] = NULL;
+    return self;
+}
+
+void AvmTypeBuilderSetName(AvmTypeBuilder* self, str name)
+{
+    pre
+    {
+        assert(self != NULL);
+        assert(name != NULL);
+    }
+
+    base->_name = name;
+}
+
+void AvmTypeBuilderSetDtor(AvmTypeBuilder* self, AvmDtor dtor)
+{
+    pre
+    {
+        assert(self != NULL);
+        assert(dtor != NULL);
+    }
+
+    base->_vptr[FnEntryDtor] = (AvmFunction)dtor;
+}
+
+void AvmTypeBuilderAddMember(AvmTypeBuilder* self, AvmType* type)
+{
+    pre
+    {
+        assert(self != NULL);
+        assert(type != NULL);
+    }
+
+    base->_size += AvmTypeGetSize(type);
+}
