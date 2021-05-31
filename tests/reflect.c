8e86d749ebe137eecfbd858c497ff4024e989120
Vasilis Mylonas <vasilismylonas@protonmail.com>
2021-05-31 18:54:58 +0300

    Test cleanup.

--- tests/reflect.c
+++ tests/reflect.c
@@ -1,26 +1,66 @@
 #include "avium/reflect.h"
+
 #include "avium/string.h"
 #include "avium/testing.h"
 #include "avium/typeinfo.h"
 
 #include <string.h>
 
+void TestModuleHasSymbol()
+{
+    const AvmModule* module = AvmModuleGetCurrent();
+    const str realSymbol = "AvmStringFrom";
+    const str fakeSymbol = "AvmStringFromFake";
+
+    assert(AvmModuleHasSymbol(module, realSymbol));
+    assert(!AvmModuleHasSymbol(module, fakeSymbol));
+}
+
+void TestModuleGetFunction()
+{
+    const AvmModule* module = AvmModuleGetCurrent();
+    const str functionName = "AvmModuleLoad";
+    const AvmFunction function = (AvmFunction)AvmModuleLoad;
+
+    assert(function == AvmModuleGetFunction(module, functionName));
+}
+
+void TestModuleGetType()
+{
+    const AvmModule* module = AvmModuleGetCurrent();
+    const str typeName = "AvmString";
+    const AvmType* const type = typeid(AvmString);
+
+    assert(type == AvmModuleGetType(module, typeName));
+}
+
+void TestModuleGetName()
+{
+    const AvmModule* module = AvmModuleGetCurrent();
+    const str moduleName = "reflect";
+
+    assert(strcmp(moduleName, AvmModuleGetName(module)) == 0);
+}
+
+void TestReflectLoadType()
+{
+    const AvmType* const type = typeid(AvmLocation);
+    const str qualifiedName = "avm.core@AvmLocation";
+
+    assert(type == AvmReflectLoadType(qualifiedName));
+}
+
+void TestModuleGetVariable()
+{
+    // TODO
+}
+
 void main()
 {
-    const AvmModule* mod = AvmModuleGetCurrent();
-
-    assert(AvmModuleHasSymbol(mod, "AvmStringFrom"));
-    assert(!AvmModuleHasSymbol(mod, "AvmString"));
-
-    AvmFunction func = AvmModuleGetFunction(mod, "AvmModuleLoad");
-    assert(func == (AvmFunction)AvmModuleLoad);
-
-    const AvmType* type = AvmModuleGetType(mod, "AvmString");
-    assert(type == typeid(AvmString));
-
-    str name = AvmModuleGetName(mod);
-    assert(strcmp(name, "reflect") == 0);
-
-    const AvmType* type2 = AvmReflectLoadType("avm.core@AvmLocation");
-    assert(type2 == typeid(AvmLocation));
+    TestModuleHasSymbol();
+    TestModuleGetFunction();
+    TestModuleGetType();
+    TestModuleGetName();
+    TestReflectLoadType();
+    TestModuleGetVariable();
 }
